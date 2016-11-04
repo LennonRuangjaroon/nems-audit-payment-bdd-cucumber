60ca8a1016f857eb51557d144e8d559783c433fc
Werawad_Rua <Werawad_Rua@truecorp.co.th>
2016-11-04 15:19:28 +0700

    Create project.

---
+++ src/main/java/com/truecorp/pushad/bdd/PushadApiBddCucumberApplication.java
@@ -0,0 +1,12 @@
+package com.truecorp.pushad.bdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushadApiBddCucumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PushadApiBddCucumberApplication.class, args);
+	}
+}
