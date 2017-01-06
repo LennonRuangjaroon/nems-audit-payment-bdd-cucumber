4fc299e0149db80556fe809026b372478eecb277
Werawad_Rua <Werawad_Rua@truecorp.co.th>
2017-01-06 12:22:42 +0700

    add max

--- src/test/java/com/truecorp/pushad/bdd/feature/advertisement/AdvertisementSteps.java
+++ src/test/java/com/truecorp/pushad/bdd/feature/advertisement/AdvertisementSteps.java
@@ -75,9 +75,9 @@ public class AdvertisementSteps extends AbstractTest {
     public void user_login_pushad_with(String username, String password)
             throws MalformedURLException {
 
-        webDriver.manage().window().maximize();
-        
         webDriver = testHelper.getDriver();
+        
+        webDriver.manage().window().maximize();
 
         webDriver.get(url + "/login");
         userNameEle = webDriver
