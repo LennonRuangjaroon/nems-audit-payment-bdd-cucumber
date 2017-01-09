d5f4171c630c25a078667875e9b0127609d36cff
Werawad_Rua <Werawad_Rua@truecorp.co.th>
2017-01-09 14:45:51 +0700

    test upload

--- src/test/java/com/truecorp/pushad/bdd/feature/advertisement/AdvertisementTest.java
+++ src/test/java/com/truecorp/pushad/bdd/feature/advertisement/AdvertisementTest.java
@@ -9,7 +9,7 @@ import cucumber.api.junit.Cucumber;
 /**
  * @author Lennon
  */
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:feature/advertisement.feature", plugin = {
         "html:target/cucumber-html-report-advertisement",
         "json:target/cucumber-json-report-advertisement.json"
