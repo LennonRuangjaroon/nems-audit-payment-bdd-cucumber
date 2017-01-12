68509ac64004b1f94dbd85d48d9960faf7e03614
Werawad_Rua <Werawad_Rua@truecorp.co.th>
2017-01-12 14:19:15 +0700

    change remote driver

--- src/test/java/com/truecorp/pushad/bdd/TestHelper.java
+++ src/test/java/com/truecorp/pushad/bdd/TestHelper.java
@@ -1,3 +1,4 @@
+
 package com.truecorp.pushad.bdd;
 
 import java.net.MalformedURLException;
@@ -13,9 +14,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 /**
- * 
  * @author Lennon
- *
  */
 @Component
 @ConfigurationProperties(prefix = "pushad")
@@ -24,15 +23,17 @@ public class TestHelper {
     private static final Logger logger = LoggerFactory.getLogger(TestHelper.class);
     private static final String SELENIUM_PORT_4444_TCP_ADD = "SELENIUM_PORT_4444_TCP_ADDR";
     private static final String DSELENIUM_PORT_4444_TCP_POR = "SELENIUM_PORT_4444_TCP_PORT";
-    
+
     private String profile;
 
     public WebDriver getDriver() throws MalformedURLException {
         logger.info("profile : {}", profile);
 
         if (profile.equals("development")) {
-            String chromeRemoteDriver = "http://" + System.getenv(SELENIUM_PORT_4444_TCP_ADD)
-                    + ':' + System.getenv(DSELENIUM_PORT_4444_TCP_POR) + "/wd/hub";
+            // String chromeRemoteDriver = "http://" + System.getenv(SELENIUM_PORT_4444_TCP_ADD)
+            // + ':' + System.getenv(DSELENIUM_PORT_4444_TCP_POR) + "/wd/hub";
+
+            String chromeRemoteDriver = "http://172.17.181.204:33374/wd/hub";
             logger.info("chromeRemoteDriver : {}", chromeRemoteDriver);
 
             return new RemoteWebDriver(new URL(chromeRemoteDriver),
@@ -40,6 +41,7 @@ public class TestHelper {
         } else {
             return new ChromeDriver();
         }
+
     }
 
     public String getProfile() {
