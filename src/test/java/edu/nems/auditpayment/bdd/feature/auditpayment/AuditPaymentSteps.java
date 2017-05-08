dc2fe571fc3487fabb32ab48a52cd745ae2fe623
(Lennon)Werawad Runagjaroon <Werawad_Rua@truecorp.co.th>
2017-05-08 19:48:17 +0700

    Add one Scenario

--- src/test/java/edu/nems/auditpayment/bdd/feature/auditpayment/AuditPaymentSteps.java
+++ src/test/java/edu/nems/auditpayment/bdd/feature/auditpayment/AuditPaymentSteps.java
@@ -7,7 +7,9 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import edu.nems.auditpayment.bdd.AbstractTest;
 import org.junit.After;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -39,19 +41,62 @@ public class AuditPaymentSteps extends AbstractTest {
         webDriver.quit();
     }
 
-    @Given("^user open nems site$")
-    public void user_open_nems_site() throws InterruptedException {
+    @Given("^User is on the Payment page$")
+    public void user_is_on_the_Payment_page() {
         webDriver.manage().window().maximize();
         webDriver.get(url + "/payment/Payment");
-        Thread.sleep(3000);
     }
 
-    @When("^user click menu$")
-    public void user_click_menu() {
+    @When("^User input data field Reference Number Payment with \"([^\"]*)\"$")
+    public void user_input_data_field_Reference_Number_Payment_with(String input) {
+        webDriver.findElement(By.name("pay_bill")).sendKeys(input);
     }
 
-    @Then("^user click save$")
-    public void user_click_save() {
+    @When("^User input data field Date of payment with \"([^\"]*)\"$")
+    public void user_input_data_field_Date_of_payment_with(String input) {
+        webDriver.findElement(By.name("pay_date")).sendKeys(input);
+    }
+
+    @When("^User input data field Payment status with \"([^\"]*)\"$")
+    public void user_input_data_field_Payment_status_with(String input) {
+        switch (input) {
+            case "ชำระเรียบร้อย":
+                input = "1";
+                break;
+            case "ไม่พบข้อมูล/ไม่มีข้อมูลชำระเงิน":
+                input = "2";
+                break;
+            case "ชำระไม่ครบตามจำนวน":
+                input = "3";
+                break;
+        }
+        webDriver.findElement(By.name("pay_bs_id")).sendKeys(input);
+    }
+
+    @When("^User input data field Amount with \"([^\"]*)\"$")
+    public void user_input_data_field_Amount_with(String input) {
+        webDriver.findElement(By.name("pay_amount")).sendKeys(input);
+    }
+
+    @When("^User input data field Payee with \"([^\"]*)\"$")
+    public void user_input_data_field_Payee_with(String input) {
+        webDriver.findElement(By.name("pay_receiver")).sendKeys(input);
+    }
+
+    @When("^User click save button to insert data$")
+    public void user_click_save_button_to_insert_data() {
+        webDriver.findElement(By.xpath("/html/body/div/section/section[1]/div/div/div[2]/form/fieldset[2]/div/div/button[1]")).click();
+
+    }
+
+    @Then("^User should see record the data successfully and then display the data in the table\\.$")
+    public void user_should_see_record_the_data_successfully_and_then_display_the_data_in_the_table() throws InterruptedException {
+        webDriver.get(url + "/payment/Payment");
+        Thread.sleep(1000);
+
+        WebElement payReceiver = webDriver.findElement(By.xpath(".//*[@id='datatable3']/tbody/tr/td[7]/center"));
+        assertNotNull(payReceiver);
+        assertEquals("Admin Test", payReceiver.getText());
     }
 
     public String getUrl() {
--- src/test/resources/feature/auditPayment.feature
+++ src/test/resources/feature/auditPayment.feature
@@ -1,7 +1,12 @@
-Feature: Testing the AuditPayment Module
-  Users should be able Open Web Site NEMS.
+Feature: Testing the AuditPayment module
+  Users should be able save data audit payment
 
-  Scenario: Users should be able open module audit payment successful
-    Given user open nems site
-    When user click menu
-    Then user click save
+  Scenario: Insert data with fill all fields should be successfully.
+    Given User is on the Payment page
+    When User input data field Reference Number Payment with "000001"
+    And User input data field Date of payment with "01/03/2017"
+    And User input data field Payment status with "ชำระเรียบร้อย"
+    And User input data field Amount with "300"
+    And User input data field Payee with "Admin Test"
+    And User click save button to insert data
+    Then User should see record the data successfully and then display the data in the table.
